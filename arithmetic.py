b8296e60ddb1247b7ad2ce0c9d713b7831c69e8f
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:36:06 -0700

    Added docstrings

--- arithmetic.py
+++ arithmetic.py
@@ -5,22 +5,38 @@ def add(num1, num2):
     return num1 + num2
 
 def subtract(num1, num2):
+    '''When: you want to subtract two numbers
+    Why: when you want to subtract two numbers'''
     return add(num1, num2 * -1)
 
 def multiply(num1, num2):
+    '''When: you want to multiply two numbers
+    Why: when you want to multiply two numbers'''
     return num1 * num2
 
 def divide(num1, num2):
+    '''When: you want to divide two numbers
+    Why: when you want to divide two numbers'''
     return num1 / num2
 
 def square(num1):
+    '''When: you want to multiply a number by itself
+    Why: when you want to multiply a number by itself'''
     return num1 ** 2
 
 def cube(num1):
+    '''When: you want to cube a number
+    Why: when you want to cube a number'''
     return num1 ** 3
 
 def power(num1, num2):
+    '''When: you want to raise a number to another number
+    Why: when you want to raise a number to another number'''
     return num1 ** num2
 
 def mod(num1, num2):
+    '''When: you want to perform modulous division
+    Why: when you want to perform modulous division'''
     return num1 % num2
+
+

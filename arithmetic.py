00197f58d807555d295117ae406fd1f81cc72638
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:19:10 -0700

    initial commit

---
+++ arithmetic.py
@@ -0,0 +1,26 @@
+def add(num1, num2):
+    '''When: when you want to add two numbers
+    Why: to add two numbers
+    '''
+    return num1 + num2
+
+def subtract(num1, num2):
+    return add(num1, num2 * -1)
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def square(num1):
+    return num1 ** 2
+
+def cube(num1):
+    return num1 ** 3
+
+def power(num1, num2):
+    return num1 ** num2
+
+def mod(num1, num2):
+    return num1 % num2
